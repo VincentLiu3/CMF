9142a9c8696bc5705e16667dfd4555cb7863c7ff
Vincent Liu <b01701150@ntu.edu.tw>
2017-06-01 17:37:28 +0800

    add argparse

--- cmf.py
+++ cmf.py
@@ -1,20 +1,39 @@
 '''
 Collective Matrix Factorization
-Vincent Liu
 2017 May
 '''
 import numpy
 import time
 import scipy.sparse
+import argparse
 from anewton import logistic, newton_update
-from utils import read_triple_data, read_binary_data, read_data, RMSE, MAE, check_modes, get_config
+from utils import *
 
-def learn(Xs, Xstst, rc_schema, alphas, modes, K, reg, learn_rate, max_iter=100, tol=0.4):
+def parse_args():
+    # python3 cmf.py --alphas '0.8-0.1-0.1' --k 10 --reg 0.1 --lr 0.1 --tol 1.0 --verbose 1
+    parser = argparse.ArgumentParser(description = 'Collective Matrix Factorization')
+    parser.add_argument('--train' , type = str, default = 'data/ml-1m/train.txt', help = 'Training file')
+    parser.add_argument('--test' , type = str, default = 'data/ml-1m/test.txt', help = 'Testing file')
+    parser.add_argument('--user' , type = str, default = 'data/ml-1m/user.txt', help = 'User features (Optional)')
+    parser.add_argument('--item' , type = str, default = 'data/ml-1m/item.txt', help = 'Item features (Optional)')
+    parser.add_argument('--save', type = str, default = 'result.txt', help = 'File where fianl result will be saved')
+    
+    parser.add_argument('--alphas' , type = str, default = '0.8-0.1-0.1', help = 'Alpha in [0, 1] weights the relative importance of relations')
+    
+    parser.add_argument('--k', type = int, default = 10, help = 'Dimension of latent fectors')
+    parser.add_argument('--reg', type = float, default = 0.1, help = 'Regularization for latent facotrs')
+    parser.add_argument('--lr', type = float, default = 0.1, help = 'Initial learning rate for training')
+    
+    parser.add_argument('--max_iter', type = int, default = 100, help = 'Max training iteration')
+    parser.add_argument('--tol', type = float, default = 0.5, help = 'Tolerant for change in training loss')
+    parser.add_argument('--verbose', type = int, default = 1, help = 'Verbose or not')
+    return parser.parse_args()
+
+def learn(Xs, Xstst, rc_schema, modes, alphas, K, reg, learn_rate, max_iter, tol, verbose):
     assert(rc_schema.shape[0] == len(Xs) and rc_schema.shape[1] == 2) # schema match data
     assert(numpy.all(rc_schema[:, 0] != rc_schema[:, 1])) # should not have symmetric relations
     assert(rc_schema.shape[0] == len(alphas))
     assert(rc_schema.shape[0] == len(modes))
-    check_modes(modes)
 
     Xts = [None] * len(Xs)
     for i in range(len(Xs)):
@@ -23,35 +42,38 @@ def learn(Xs, Xstst, rc_schema, alphas, modes, K, reg, learn_rate, max_iter=100,
             Xs[i] = scipy.sparse.csc_matrix(Xs[i]) # no Transpose
         if Xstst[i] is not None:
             Xstst[i] = scipy.sparse.csc_matrix(Xstst[i])
-    
+
     [S, Ns] = get_config(Xs, rc_schema)
 
     # randomly initialize factor matrices with small values
     Us = [None] * S
     for i in range(S):
-        Us[i] = numpy.random.rand(Ns[i], K) / K
+        Us[i] = numpy.random.rand(Ns[i], K) / numpy.sqrt(K)
 
     prev_loss = loss(Us, Xs, rc_schema, modes, alphas, reg)
     i = 0
     while i < max_iter:
         i += 1
+        # training 
         tic = time.time()
         for t in range(S):
             newton_update(Us, Xs, Xts, rc_schema, alphas, modes, K, reg, learn_rate, Ns, t)
-        toc = time.time()
-
-        print("[CMF] Iteration {}/{}. Time: {:.1f}".format(i, max_iter, toc - tic))
-
+        
+        # evaluation
         training_loss = loss(Us, Xs, rc_schema, modes, alphas, reg)
         change_rate = (prev_loss-training_loss)/prev_loss * 100
         prev_loss = training_loss
-        
-        Ystst = predict(Us, Xstst, rc_schema, modes)
-        testing_loss = RMSE(Xstst[0], Ystst[0])
-        print("[CMF] Training Loss: {:.2f} (change {:.2f}%). Testing RMSE: {:.2f}".format(training_loss, change_rate, testing_loss))
+
+        if verbose == 1:
+            toc = time.time()
+            Ystst = predict(Us, Xstst, rc_schema, modes)
+            testing_loss = RMSE(Xstst[0], Ystst[0])
+            print("[CMF] Iteration {}/{}. Time: {:.1f}".format(i, max_iter, toc - tic))
+            print("[CMF] Training Loss: {:.2f} (change {:.2f}%). Testing RMSE: {:.2f}".format(training_loss, change_rate, testing_loss))
 
         if change_rate < tol and i != 1:
-            print("[CMF] Early terminate due to insufficient change in training loss!")
+            if verbose == 1:
+                print("[CMF] Early stopping due to insufficient change in training loss!")
             break
 
     return Us
@@ -139,50 +161,51 @@ def predict(Us, Xs, rc_schema, modes):
 
     return Ys
 
-def run_cmf(Xs_trn, Xs_tst, rc_schema, alphas, modes, K, reg, learn_rate, T):
-    print('----------------Running CMF----------------')
-    Us = learn(Xs_trn, Xs_tst, rc_schema, alphas, modes, K, reg, learn_rate, T)
-
+def run_cmf(Xs_trn, Xs_tst, rc_schema, modes, args):
+    '''
+    run cmf and return rmse
+    '''
+    alphas = string2list(args.alphas, len(modes))
+    if args.verbose == 1:
+        start_time = time.time()
+        print('[Settings] k = {}. reg = {}. lr = {}. alpha = {}'.format(args.k, args.reg, args.lr, alphas))
+    
+    # training
+    Us = learn(Xs_trn, Xs_tst, rc_schema, modes, alphas, args.k, args.reg, args.lr, args.max_iter, args.tol, args.verbose)
+    # Evaluation
     Ys_tst = predict(Us, Xs_tst, rc_schema, modes)
-    X = Xs_tst[0]
-    Y = Ys_tst[0]
-    print(X.data)
-    print(Y.data)
+    rmse = RMSE(Xs_tst[0], Ys_tst[0])
 
-    print('[CMF] Final Results:')
-    print('[CMF] K = {}, reg = {:.5f}, learning rate = {:.5f} '.format(K, reg, learn_rate) )
-    print('[CMF] RMSE = {:.4f} , MAE= {:.4f}'.format(RMSE(X, Y), MAE(X, Y)) )
+    if args.verbose == 1:
+        end_time = time.time()
+        print('[Results] Total Running Time: {:.0f} seconds'.format(end_time - start_time) )
+    
+    return rmse
+    
 
 if __name__ == "__main__":
-    start_time = time.time()
-    [Xs_trn, Xs_tst] = read_triple_data('ml-1m')
-    end_time = time.time()
-    print('[CMF] Finished loading data. Time: {:.1f} seconds'.format(end_time - start_time) )
+    args = parse_args()
+    [Xs_trn, Xs_tst] = read_triple_data(args.train, args.test, args.user, args.item)
 
-    rc_schema = numpy.array([[0, 1], [0, 2], [1, 3]]) 
+    #### Need to modify this part for your own dataset ####
+    rc_schema = numpy.array([[0, 1], [0, 2], [1, 3]])
     # [row entity number, column entity number]
     # 0=user, 1=item, 2=userFeat, 3=itemFeat
 
-    [S, Ns] = get_config(Xs_trn, rc_schema)
-    print('----------------Data Summary----------------')
-    print('[CMF] Number of entities = {}'.format(S))
-    print('[CMF] Number of relations = {}'.format(len(Xs_trn)))
-    print('[CMF] Number of instnace for each entity = {}'.format(Ns))
-    print('[CMF] Training size = {}. Testing size = {}'.format(Xs_trn[0].size, Xs_tst[0].size))
-    
-    alphas = [0.8, 0.1, 0.1] 
-    # alpha in [0, 1] weights the relative importance of relations
-
     modes = ['sparse', 'log_dense', 'log_dense']
+    check_modes(modes)
     # modes of each relation: sparse, dense or log_dense
-    # dense if Wij = 1 for all ij or sparse if Wij = 1 if Xij>0
-    # log if F = logistic
+    # dense if Wij = 1 for all ij 
+    # sparse if Wij = 1 if Xij>0
+    # log if link function = logistic
 
-    # reg = regulaizer
-    # K = number of latent factors
-    # learn_rate = learn rate
-    # T = max number of iterations
-    run_cmf(Xs_trn, Xs_tst, rc_schema, alphas, modes, K = 20, reg = 0.00001, learn_rate = 0.0062, T = 100)
+    if args.verbose == 1:
+        [S, Ns] = get_config(Xs_trn, rc_schema)
+        print('[Data] Number of relations = {}'.format(len(Xs_trn)))
+        print('[Data] Number of instnace for each entity = {}'.format(Ns))
+        print('[Data] Training size = {}. Testing size = {}'.format(Xs_trn[0].size, Xs_tst[0].size))
 
-    end_time = time.time()
-    print('[CMF] Total Running Time: {:.0f} seconds'.format(end_time - start_time) )
+    rmse = run_cmf(Xs_trn, Xs_tst, rc_schema, modes, args)
+    print('[Results] RMSE = {:.4f}'.format(rmse))
+    save_result(args, rmse)
+    
